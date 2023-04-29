ba8b0baf6ff8358075973849e7b2b605f06cf7fa
Marco Scheuerer <MarcoScheuerer1978@gmail.com>
2023-04-29 15:26:36 +0200

    Containers Map and Set

---
+++ 07_01_Container/Map.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <map>
+#include <cstdint>
+#include <string>
+
+int main()
+{
+    auto my_map = std::map<std::string, std::uint32_t> { };
+
+    my_map["Jan"] = 28;
+    my_map["Dennis"] = 31;
+    my_map["Lisa"] = 33;
+
+    my_map["Jan"] = 29;
+
+    for (const auto &val : my_map)
+    {
+        std::cout << val.first << ": " << val.second << '\n';
+    }
+
+    std::cout << my_map["Jan"] << '\n';
+
+    // C++17
+    auto search_str = "Jan";
+    const auto search_it = my_map.find(search_str);
+    bool found_key = search_it != my_map.end();
+
+    // C++20
+    bool found_key_2 = my_map.contains(search_str);
+
+    if (found_key_2)
+        std::cout << "Key Jan found!\n";
+
+    return 0;
+}
--- 07_01_Container/StructuredBindings.cc
+++ 07_01_Container/StructuredBindings.cc
@@ -2,3 +2,40 @@
 #include <map>
 #include <string>
 #include <tuple>
+
+void get_data_old(int &a, std::string &b, double &c)
+{
+    a = 1337;
+    b = "data";
+    c = 1337.0;
+}
+
+std::tuple<int, std::string, double> get_data()
+{
+    return std::make_tuple(1337, "data", 1337.0);
+}
+
+int main()
+{
+    int a;
+    std::string b;
+    double c;
+
+    get_data_old(a, b, c);
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << c << std::endl;
+    std::cout << std::endl;
+
+    // Old access by get<T>()
+    std::cout << std::get<0>(get_data()) << std::endl;
+    std::cout << std::get<1>(get_data()) << std::endl;
+    std::cout << std::get<2>(get_data()) << std::endl;
+    std::cout << std::endl;
+
+    // New access by Structured Binding (better readable, easier to use)
+    const auto [id, name, score] = get_data();
+    std::cout << id << std::endl;
+    std::cout << name << std::endl;
+    std::cout << score << std::endl;
+}
