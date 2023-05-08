74b94b91315889f5020ad19c494e7b621a170acd
Marco Scheuerer <MarcoScheuerer1978@gmail.com>
2023-05-08 20:50:21 +0200

    Threading Basics 1

---
+++ 12_Threading/ThreadBasics1.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <thread>
+
+void function()
+{
+    const auto main_id = std::this_thread::get_id();
+    std::cout << "Thread ID: " << main_id << '\n';
+
+    std::cout << "Working\n";
+}
+
+int main()
+{
+    const auto main_id = std::this_thread::get_id();
+    std::cout << "Main Thread ID: " << main_id << '\n';
+
+    auto t1 = std::thread(function);
+
+    if (t1.joinable())
+        t1.join();
+
+    return 0;
+}
